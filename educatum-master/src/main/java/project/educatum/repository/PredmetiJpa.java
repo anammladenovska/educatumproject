def1122fd4b031b12d11d64d1ac0b8684e12d28d
Marija Micevska <marija_micevska@outlook.com>
2022-02-03 00:11:29 +0200

    Changes

--- educatum-master/src/main/java/project/educatum/repository/PredmetiJpa.java
+++ educatum-master/src/main/java/project/educatum/repository/PredmetiJpa.java
@@ -1,11 +1,13 @@
 package project.educatum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.educatum.model.Predmeti;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PredmetiJpa extends JpaRepository<Predmeti, Integer> {
     List<Predmeti> findAllByImeContainingIgnoreCase(String ime);
     Optional<Predmeti> findById(Integer id);
