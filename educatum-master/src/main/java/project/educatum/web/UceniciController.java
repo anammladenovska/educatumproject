a741febca446efef4cfd3cced8097c4b73f12ebd
Ana Mladenovska <80364724+anammladenovska@users.noreply.github.com>
2022-02-13 23:56:52 +0100

    add2

--- educatum-master/src/main/java/project/educatum/web/UceniciController.java
+++ educatum-master/src/main/java/project/educatum/web/UceniciController.java
@@ -1,20 +1,17 @@
 package project.educatum.web;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import project.educatum.model.Nastavnici;
-import project.educatum.model.Predmeti;
 import project.educatum.model.Ucenici;
+import project.educatum.service.NastavniciService;
 import project.educatum.service.PredmetiService;
 import project.educatum.service.UceniciService;
 import project.educatum.service.ZainteresiraniZaService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/ucenici", method = {RequestMethod.POST, RequestMethod.DELETE, RequestMethod.GET})
@@ -23,12 +20,30 @@ public class UceniciController {
     private final UceniciService uceniciService;
     private final PredmetiService predmetiService;
     private final ZainteresiraniZaService zainteresiraniZaService;
+    private final NastavniciService nastavniciService;
 
-    public UceniciController(UceniciService uceniciService, PredmetiService predmetiService, ZainteresiraniZaService zainteresiraniZaService) {
+    public UceniciController(UceniciService uceniciService, PredmetiService predmetiService, ZainteresiraniZaService zainteresiraniZaService, NastavniciService nastavniciService) {
         this.uceniciService = uceniciService;
         this.predmetiService = predmetiService;
         this.zainteresiraniZaService = zainteresiraniZaService;
+        this.nastavniciService = nastavniciService;
     }
+
+    @GetMapping("/listSubjectsTeachers")
+    public String listSubjectsTeachers(Model model){
+//        model.addAttribute("predmeti", predmetiService.findAll());
+//        model.addAttribute("nastavnici", nastavniciService.getAllTeachersBySubject(Integer.valueOf(subject)));
+        return "listSubTeachStudents";
+    }
+
+    @PostMapping("/teachers")
+    public String getAllTeachersBySubject(Model model, @RequestParam String predmetId) {
+        model.addAttribute("predmet", predmetiService.findById(Integer.valueOf(predmetId)));
+        model.addAttribute("predmeti", predmetiService.findAll());
+        model.addAttribute("nastavnici", nastavniciService.getAllTeachersBySubject(Integer.valueOf(predmetId)));
+        return "listSubTeachStudents";
+    }
+
     @PostMapping("/delete/{id}")
     public String deleteStudent(@PathVariable String id){
         uceniciService.delete(Integer.parseInt(id));
@@ -50,4 +65,6 @@ public class UceniciController {
         zainteresiraniZaService.addSubjectStudent(Integer.valueOf(predmetId),u.getId(),localDate );
         return "redirect:/zaintesesiranZaPredmet";
     }
+
+
 }
